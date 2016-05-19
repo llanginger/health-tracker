0b422cbab41834095a548eb05482639234043d26
llanginger <llanginger@gmail.com>
2016-05-19 11:31:27 -0700

    Rough Autocomplete Working

--- js/views/searchView.js
+++ js/views/searchView.js
@@ -1,7 +1,8 @@
 var app = app || {};
 
 var nSets = {
-  initUrl: "https://api.nutritionix.com/v1_1/search/",
+  searchUrl: "https://api.nutritionix.com/v1_1/search/",
+  autoUrl: "https://api.nutritionix.com/v2/autocomplete?q=",
   phrase: "Tacos",
   appId: "appId=02cd80d0",
   appKey: "appKey=421b1fb27190316a6585e273b648dd6f",
@@ -10,13 +11,32 @@ var nSets = {
   fields: "fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat"
 }
 
+// TODO Autocomplete as people are typing. Once they click, send the autocompleted-text to the nutri search function to retrieve the final options. Use a <datalist> tag in template and populate with results from the autocomplete function.
+$(function(){
+  $("body").click(function( event ) {
+    console.log(event.target);
+  })
+
+  $('#search-bar').on('change', function() {
+  var value = $(this).val();
+  alert(value);
+});
+})
+
+
 app.SearchView = Backbone.View.extend({
 
   el: "#search-div",
 
   events: {
     "click input": "edit",
+    "click .food-option": "printOption",
     "keypress #search-bar": "updateOnEnter"
+  },
+
+  printOption: function() {
+    this.$option = this.$("option");
+    console.log(this.$option);
   },
 
   initialize: function() {
@@ -32,6 +52,7 @@ app.SearchView = Backbone.View.extend({
   },
 
   edit: function() {
+    this.$input.val("");
     this.$el.addClass( "editing" )
     console.log("Edit fired");
   },
@@ -42,7 +63,7 @@ app.SearchView = Backbone.View.extend({
     if ( value ) {
       // this.model.save({title: value});
       console.log(value);
-      $.ajax(nSets.initUrl + value + "?" + nSets.fields + "&" + nSets.appId + "&" + nSets.appKey)
+      $.ajax(nSets.searchUrl + value + "?" + nSets.fields + "&" + nSets.appId + "&" + nSets.appKey)
         .fail(function( data ) {
           console.log("Nutri api call failed");
         })
@@ -54,9 +75,29 @@ app.SearchView = Backbone.View.extend({
     }
   },
 
+  autocompleteBar: function() {
+
+    var value = this.$input.val().trim();
+    $.ajax(nSets.autoUrl + value + "&" + nSets.appId + "&" + nSets.appKey)
+      .fail(function( data ) {
+        console.log("Nutri api call failed");
+      })
+      .done(function( data ) {
+        $("option").remove();
+        for (var i = 0; i < data.length; i++) {
+          $("#food-list").append("<option class='food-option' value='" + data[i].text + "' />");
+        }
+        console.log(data);
+        console.log(value);
+      })
+
+  },
+
   updateOnEnter: function( e ) {
     if (e.which === ENTER_KEY ) {
       this.close();
+    } else {
+      this.autocompleteBar();
     }
   },
 
