2f653360557fc16ecc9c94e724d334dc3410678c
llanginger <llanginger@gmail.com>
2016-06-15 22:58:37 -0700

    Modified FoodItems to use local storage, getting zombie views

--- js/app.js
+++ js/app.js
@@ -4,3 +4,9 @@ var ENTER_KEY = 13;
 $(function() {
   new app.AppView();
 });
+
+// Move app.FoodItems to localstorage so that this function will not nuke other users's models
+
+// window.onbeforeunload = function(e) {
+//   app.FoodItems.reset();
+// };
